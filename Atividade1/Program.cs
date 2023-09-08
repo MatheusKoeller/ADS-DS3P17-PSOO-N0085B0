3a3b9471ace0301328e3df473ff4d08f927d65e5
Matheus Mação <117746452+MatheusKoeller@users.noreply.github.com>
2023-09-08 20:17:39 -0300

    Add files via upload

---
+++ Atividade1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Aluno
+{
+    public string Nome { get; set; }
+    public string RA { get; set; }
+
+    public Aluno(string nome, string ra)
+    {
+        Nome = nome;
+        RA = ra;
+    }
+
+    public virtual void ValidarRA(string ra)
+    {
+        if (RA == ra)
+        {
+            Console.WriteLine($"Nome do aluno: {Nome}");
+        }
+        else
+        {
+            Console.WriteLine("RA não corresponde ao aluno.");
+        }
+    }
+}
+
+class AlunoComValidacao : Aluno
+{
+    public AlunoComValidacao(string nome, string ra) : base(nome, ra)
+    {
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AlunoComValidacao aluno = new AlunoComValidacao("Matheus Mação", "N0085B0");
+
+        Console.Write("Digite um RA: ");
+        string raDigitado = Console.ReadLine();
+        aluno.ValidarRA(raDigitado);
+    }
+    
+}
